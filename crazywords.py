f114aece7e9144cb8554b43ef87116627b441adb
sjo <arnau.fernandez.7e4@itb.cat>
2024-05-02 11:35:46 +0200

    2/05/2024 paraules boges

---
+++ crazywords.py
@@ -0,0 +1,24 @@
+import random
+import main
+
+def crazywords(palabra):
+    if len(palabra) <= 2:
+        return palabra
+
+    # Obtenemos la primera y última letra de la palabra
+    primera_letra = palabra[0]
+    ultima_letra = palabra[-1]
+
+    # Convertimos las letras intermedias en una lista
+    letras_medio = list(palabra[1:-1])
+
+    # Aleatorizamos el orden de las letras intermedias
+    random.shuffle(letras_medio)
+
+    # Creamos la palabra "loca" uniendo todas las letras
+    palabra_loca = primera_letra
+    for letra in letras_medio:
+        palabra_loca += letra
+    palabra_loca += ultima_letra
+
+    return palabra_loca
